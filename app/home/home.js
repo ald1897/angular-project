c0049cfb5fa9247639794eaef336fa9f583fbe61
Alex Dickinson <alexdickinson04@gmail.com>
2017-04-23 21:07:11 -0400

    Added Google Map API connection, Still need to add location information

--- app/home/home.js
+++ app/home/home.js
@@ -12,3 +12,67 @@ angular.module('myApp.home', ['ngRoute'])
 .controller('homeCtrl', [function() {
 
 }]);
+
+
+    function initMap() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap',
+        center:new google.maps.LatLng(18.508742,-68.120850),
+        zoom:1,
+    };
+                    
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
+        
+    // Multiple Markers
+    var markers = [
+        ['Presidential Suites, Punta Cana', 18.690674, -68.422102],
+        ['Copely Square Hotel, Boston', 42.348307, -71.078606]
+    ];
+                        
+    // Info Window Content
+    var infoWindowContent = [
+        ['<div class="info_content">' +
+        '<h3 class="bg-primary alert"><a href="https://photos.google.com/album/AF1QipNzlURSgyAZARHTu3nmzyc19zVMoWdAqsrGRJJa">Punta Cana</a></h3>' +
+        '<p>Presidential Suites Punta Cana by Lifestyle is set within the lush tropical area of Punta Cana, with a Caribbean hideaway vibe, and scrumptiously appointed one, two and three bedroom suites. Fully furnished living and kitchen area, with stainless steel appliances and designer furnishings set the standard for lavish comfort.</p>' +        '</div>'],
+        ['<div class="info_content">' +
+        '<h3>Palace of Westminster</h3>' +
+        '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
+        '</div>']
+    ];
+        
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+        
+        // Allow each marker to have an info window    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
+    });
+    
+}
--- app/sports/sports.js
+++ app/sports/sports.js
@@ -9,6 +9,14 @@ angular.module('myApp.sports', ['ngRoute'])
   });
 }])
 
-.controller('sportsCtrl', [function() {
+.controller('sportsCtrl', [function($scope) {
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.codecademy.com/files/samples/javascript_learn_apis.xml", false);
 
+xhr.setRequestHeader('Content-Type', 'text/xml');
+xhr.send();
+
+xmlDocument = xhr.responseXML;
+console.log(xmlDocument.childNodes['0'].textContent);
 }]);
+
